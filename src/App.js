2c1c18200c00de72956e79f08b75106fd9902c6e
steverova <71293149+steverova@users.noreply.github.com>
2022-11-17 20:53:15 -0600

    first commit front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Select from "react-select";
+import {gender, month, weight, year} from "./OptionSelect"
 
 function App() {
+
+
+  const [data, setData] = useState([]);
+
+  const handleChangeSelect = (event, name) => { 
+    const value = event.value;
+
+    console.log("name: ", name);
+    console.log("value: ", value);
+
+    setData({ ...data, [name]: value });
+  }
+
+  const submitForm = (event) => {
+    event.preventDefault();
+    console.log(JSON.stringify(data))
+  }
+
+const customStyle = {
+  control: (provided) => ({
+    ...provided,
+
+    border: "0px",
+    borderColor: "#AEBDCA",
+    boxShadow: "0 0 0 0.1rem  #AEBDCA",
+  }),
+};
+  
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container-fluid">
+        <div className="col-sm-12 col-md-8 col-lg-4 mx-auto mt-5">
+          <div className="card">
+            <div className="card-body p-5">
+              <h1 className=" cattle-title text-center">CATTLE</h1>
+              <form onSubmit={submitForm}>
+                <div className="select">
+                  <label>Año</label>
+                  <Select
+                    name="year"
+                    styles={customStyle}
+                    placeholder="Seleccione el año..."
+                    options={year}
+                    onChange={(event) => handleChangeSelect(event, "year")}
+                  />
+                </div>
+
+                <div className="select">
+                  <label>Mes</label>
+                  <Select
+                    name="month"
+                    styles={customStyle}
+                    placeholder="Seleccione el mes..."
+                    options={month}
+                    onChange={(event) => handleChangeSelect(event, "month")}
+                  />
+                </div>
+
+                <div className="select">
+                  <label>Peso</label>
+                  <Select
+                    name="weight"
+                    styles={customStyle}
+                    placeholder="Seleccione el peso..."
+                    options={weight}
+                    onChange={(event) => handleChangeSelect(event, "weight")}
+                  />
+                </div>
+
+                <div className="select">
+                  <label>Genero</label>
+                  <Select
+                    name="gender"
+                    styles={customStyle}
+                    placeholder="Seleccione el genero..."
+                    options={gender}
+                    onChange={(event) => handleChangeSelect(event, "gender")}
+                  />
+                </div>
+
+                <button className="btn btn-aqua mt-3">
+                  Buscar
+                  <i className="fa-solid fa-magnifying-glass"></i>
+                </button>
+              </form>
+              <hr></hr>
+              <p>{JSON.stringify(data)}</p>
+              <hr></hr>
+
+              <div className="mt-2">
+                <label>Resultado de la aproximación</label>
+                <input
+                  disabled
+                  style={{ backgroundColor: "white" }}
+                  styles={customStyle}
+                  className="form-control "
+                  type="text"
+                  placeholder="Resultado"
+                ></input>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/OptionSelect.js
@@ -0,0 +1,49 @@
+export const weight = [
+  { value: 1, label: "000-100" },
+  { value: 2, label: "001-150" },
+  { value: 3, label: "151-200" },
+  { value: 4, label: "201-250" },
+  { value: 5, label: "251-300" },
+  { value: 6, label: "301-350" },
+  { value: 7, label: "351-400" },
+  { value: 8, label: "401-500" },
+  { value: 9, label: "More than 500" },
+];
+
+export const month = [
+  { value: 1, label: "Enero" },
+  { value: 2, label: "Febrero" },
+  { value: 3, label: "Marzo" },
+  { value: 4, label: "Abril" },
+  { value: 5, label: "Mayo" },
+  { value: 6, label: "Junio" },
+  { value: 7, label: "Julio" },
+  { value: 8, label: "Agosto" },
+  { value: 9, label: "Setiembre" },
+  { value: 10, label: "Octubre" },
+  { value: 11, label: "Noviembre" },
+  { value: 12, label: "Dicembre" },
+];
+
+export const year = [
+  { value: 2007, label: "2007" },
+  { value: 2008, label: "2008" },
+  { value: 2009, label: "2009" },
+  { value: 2010, label: "2010" },
+  { value: 2011, label: "2011" },
+  { value: 2012, label: "2012" },
+  { value: 2013, label: "2013" },
+  { value: 2014, label: "2014" },
+  { value: 2015, label: "2015" },
+  { value: 2016, label: "2016" },
+  { value: 2017, label: "2017" },
+  { value: 2018, label: "2018" },
+  { value: 2019, label: "2019" },
+  { value: 2020, label: "2020" },
+  { value: 2021, label: "2021" },
+];
+
+export const gender = [
+  { value: 1, label: "Female" },
+  { value: 2, label: "Male" },
+];
